37abde96ca8d1a0f5f9d29057d696868729548bb
Greg Raven <gregraven@mac.com>
2023-12-22 13:07:38 -0800

    Update loadHtml.js

--- _assets/js/loadHtml.js
+++ _assets/js/loadHtml.js
@@ -1,15 +1,6 @@
-/* courtesy Chris Ferdinandi
- * https://gomakethings.com
- * based on my suggestion
- * @param {string} selector - The ID of the DOM element to load into
- * (works with any selector)
- * @param {string} path - The path of the HTML file to load
-*/
-
+/*jshint esversion: 6 */
 function loadHtml(selector, path) {
-    fetch(path).then(function (response) {
-        return response.text();
-    }).then(function (text) {
-        document.querySelector(selector).innerHTML = text;
-    });
+    fetch(path)
+        .then(response => response.text())
+        .then(text => document.querySelector(selector).innerHTML = text);
 }
